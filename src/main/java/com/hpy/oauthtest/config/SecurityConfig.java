a2a2e08faee7faecf7e78b5b9c5c49c06aa949c1
hpy <313382859@qq.com>
2023-05-09 16:36:15 +0800

    删除无用的路径

--- src/main/java/com/hpy/oauthtest/config/SecurityConfig.java
+++ src/main/java/com/hpy/oauthtest/config/SecurityConfig.java
@@ -36,19 +36,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/auth/**",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html"
-                )
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
 
                 .anyRequest()
